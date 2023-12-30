c4a17d149320a8100b8f3d8f57234f2668514e3a
techchakri <121855390+techchakri@users.noreply.github.com>
2023-12-30 10:33:57 +0000

    first home route created

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("<h1>Server is working...</h1>")
+})
+module.exports = app
--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello to codespaces")
+require("dotenv").config()
+const app = require("./app")
+const {PORT} = process.env
+app.listen(PORT, () => {
+    console.log(`Server is running at port: ${PORT}`)
+})
